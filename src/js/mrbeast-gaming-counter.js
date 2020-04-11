6f0a321315e37adbc3f54fd03df556f20c2bdfbc
RandomPerson3465 <50644934+RandomPerson3465@users.noreply.github.com>
2020-04-11 16:39:41 -0700

    Update mrbeast-gaming-counter.js

--- src/js/mrbeast-gaming-counter.js
+++ src/js/mrbeast-gaming-counter.js
@@ -2,7 +2,7 @@
 async function request() {
     let req = await fetch(`https://random3465api.glitch.me/api/mrbeastgaming`, { mode: "cors" })
     let res = await req.json().catch(error => notes.innerHTML = "An error occured.")
-    document.getElementById("name") = "MrBeast Gaming"
+    document.getElementById("name").innerHTML = "MrBeast Gaming"
     description.innerHTML = "MrBeast Gaming - Coming Soon SUBSCRIBE OR ELSE"
     count.innerHTML = Math.round(res.estimated)
     notes.innerHTML = "WARNING: This count can behave weirdly due to how it works."
