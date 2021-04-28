3fc36d23ed96f14de1a017c29d16fa1da7ea3eb8
RandomPerson3465 <50644934+RandomPerson3465@users.noreply.github.com>
2021-04-27 19:13:03 -0700

    Create default.js

---
+++ src/default.js
@@ -0,0 +1,32 @@
+window.onload = async function() {
+    const req = await fetch("https://randomperson3465.github.io/src/navbar.json")
+    const json = await req.json()
+    for (const entry of json.data) {
+        if (!entry.dropdown) {
+            var a = navbar.appendChild(document.createElement("a"))
+            a.innerText = entry.name
+            a.href = entry.link ? entry.link : "";
+        } else {
+            var a = navbar.appendChild(document.createElement("div"))
+            a.className = "dropdown";
+            var b = a.appendChild(document.createElement("button"))
+            b.innerText = entry.name;
+            b.className = "dropdown-button";
+            var c = a.appendChild(document.createElement("div"))
+            c.className = "dropdown-content";
+            for (const item of entry.items) {
+                var d = c.appendChild(document.createElement("a"))
+                d.innerText = item.name
+                d.href = item.link ? item.link : "";
+            }
+        }
+    }
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var d = document.getElementsByClassName("date")
+    for (i = 0; i < d.length; i++) {
+        const date = new Date(d[i].getAttribute("ts"))
+        d[i].innerText = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`
+    }
+    var y = document.querySelector(".year")
+    if (y) y.innerText = new Date().getFullYear();
+}
