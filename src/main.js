e78fbc4218c85520761d23e8239925364d325b62
RandomPerson3465 <50644934+RandomPerson3465@users.noreply.github.com>
2025-02-01 10:27:38 -0800

    removes ads

--- src/main.js
+++ src/main.js
@@ -59,3 +59,31 @@ if (document.querySelector(".footer-stuff-socials")) {
 if (document.querySelector("h1.title") && Math.random() < 1/3465) {
     document.querySelector("h1.title").innerText = "RandomPerson3456";
 }
+
+
+// https://stackoverflow.com/a/63260070
+// Removes shitty ads
+document.addEventListener('DOMContentLoaded', function () {
+    let disqus = document.getElementById('disqus_thread');
+
+    if (disqus) {
+        let remove_ads = setInterval(() => {
+            let iframes = document.getElementsByTagName('iframe');
+
+            for (var iframe in iframes) {
+                if (typeof iframes[iframe].src === 'undefined') {
+                    continue;
+                }
+
+                if (iframes[iframe].src.match(/(ads-iframe)|(disqusads)/gi)) {
+                    iframes[iframe].style.display = 'none';
+                    disqus.style.width = '100%';
+                }
+            }
+        }, 500);
+
+        setTimeout(function () {
+            clearInterval(remove_ads);
+        }, 5000);
+    }
+});
